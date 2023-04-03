6987d1853867a3359c33c2855ef3c2732aa4642d
Evan OShea <evanoshea21@gmail.com>
2023-04-03 16:17:49 -0700

    tenets

--- pages/tenets.js
+++ pages/tenets.js
@@ -8,6 +8,17 @@ const Tenets = () => {
     <div className={classes.bodyBox}>
 
     <p className={classes.body} >
+    1. A man of Principle—one who lives by his personal set of tenets—will have  direction, confidence, and assurance in everything he does. A man without a set of beliefs unwittingly follows a set of beliefs set by society, his parents, or the media, which rarely have his best interests in mind.
+    <br/><br/>
+
+    2. A man who can articulate his beliefs logically is a man who can positively influence the world and his own existence.
+    <br/><br/>
+
+    3. A man who can articulate his beliefs is a free-thinker, less impressionable by societal nudges that may have nefarious ulterior motives in mind; it is a man who can stand up for what he believes in despite the consequences.
+    <br/><br/>
+
+    {/* THIS IS FOR ACTUAL MENS GROUP
+
     1. Strength builds resilience against the suffering in life. Thus, Men should aim to be Strong, as weak men induce suffering upon themselves and others
     <br/><br/>
     2. Focus, Energy, and Time are the most important resources you have to protect for your success. Without persistent focus, you will never finish anything important, starting anew constantly. Without protecting your energy, your tank will run dry before you reach your destination. Without protecting your time, you will forever be on the clock improving the lives of others. By allowing others to steal your attention, drain your energy, and waste your time, you will never achieve your desirable goals to improve.
@@ -25,7 +36,8 @@ const Tenets = () => {
     6. A man who can articulate his beliefs is a free-thinker, less impressionable by societal nudges that may have nefarious ulterior motives in mind; it is a man who can stand up for what he believes in despite the consequences.
     <br/><br/>
 
-    7. Weak men cannot control their emotions. Strong men are stoic, and they perform their duty regardless of how they feel. This inspires respect and offers dependability for the children and women in their lives.</p>
+    7. Weak men cannot control their emotions. Strong men are stoic, and they perform their duty regardless of how they feel. This inspires respect and offers dependability for the children and women in their lives. */}
+    </p>
     <br/><br/>
 
 
